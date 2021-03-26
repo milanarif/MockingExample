14f4a1e984f1aaac8b15d70e6f8d2e861e821c2e
Shorl <milanarif@gmail.com>
2021-03-26 21:29:33 +0100

    Assignment 2, 100% coverage using both own tests and Mockito.

--- src/test/java/com/example/TestEmployeeManager.java
+++ src/test/java/com/example/TestEmployeeManager.java
@@ -1,27 +1,35 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.*;
 
 public class TestEmployeeManager {
-   TestEmployeeRepository er = new TestEmployeeRepository();
+
 
    @Test
    void integrationTestNumberPayed() {
-      TestBankService bs = new TestBankService();
-      EmployeeManager em = new EmployeeManager(er, bs);
-
+      EmployeeRepository mockedRepo = mock(EmployeeRepository.class);
+      when(mockedRepo.findAll()).thenReturn(List.of(new Employee("1", 2000), new Employee( "2", 3000)));
+      BankService mockedBankService = mock(BankService.class);
+      EmployeeManager em = new EmployeeManager(mockedRepo, mockedBankService);
       int expectedResult = 2;
       int actualResult = em.payEmployees();
 
       assertThat(actualResult).isEqualTo(expectedResult);
    }
 
-   @Test
+   @Test()
    void integrationTestExceptionDueToPaidFalse(){
-      TestBankServiceException bsf = new TestBankServiceException();
-      EmployeeManager em = new EmployeeManager(er, bsf);
+      TestEmployeeRepository employeeRepository = new TestEmployeeRepository();
+      TestBankServiceException bankService = new TestBankServiceException();
+      EmployeeManager em = new EmployeeManager(employeeRepository, bankService);
       int expectedResult = 0;
 
       int actualResult = em.payEmployees();
