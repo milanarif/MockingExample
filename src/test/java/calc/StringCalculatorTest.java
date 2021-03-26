2e904b72455e89e4148b9b3b02a2024e7192df62
Shorl <milanarif@gmail.com>
2021-03-26 11:33:34 +0100

    Step 2 passing tests. Unsure if allowed to introduce actual randomness to test input.

--- src/test/java/calc/StringCalculatorTest.java
+++ src/test/java/calc/StringCalculatorTest.java
@@ -1,9 +1,15 @@
 package calc;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class StringCalculatorTest {
 
@@ -36,4 +42,19 @@ class StringCalculatorTest {
 
       assertThat(actualSum).isEqualTo(expectedSum);
    }
+
+   @ParameterizedTest
+   @MethodSource
+   void sumOfUnknown(String input, int expected){
+      assertThat(stringCalculator.add(input)).isEqualTo(expected);
+   }
+
+   private static Stream<Arguments> sumOfUnknown(){
+      return Stream.of(
+              arguments("1,2,3,4", 10),
+              arguments("4,5,1,3,3,3", 19),
+              arguments("4,4,4,5,5,5,6,6,6", 45)
+      );
+   }
+
 }
